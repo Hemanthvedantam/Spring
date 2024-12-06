058f1333ed492eba24931343d580bef4e39bd8c8
Hemanth Hemanth <154527901+Hemanthvedantam@users.noreply.github.com>
2024-12-06 09:58:29 +0530

    Add files via upload

---
+++ EmailService.java
@@ -0,0 +1,27 @@
+
+package com.agriculturemanagementsystem.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    public String sendEmail(String to, String subject, String body) {
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setTo(to);
+            message.setSubject(subject);
+            message.setText(body);
+            mailSender.send(message);
+            return "Email sent successfully";
+        } catch (Exception e) {
+            return "Error while sending email: " + e.getMessage();
+        }
+    }
+}
