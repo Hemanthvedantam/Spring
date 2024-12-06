c4c71f096bf4e5b0d4653549c7bd20a876592837
Hemanth Hemanth <154527901+Hemanthvedantam@users.noreply.github.com>
2024-12-06 09:57:47 +0530

    Add files via upload

---
+++ AgricultureManagementSystemApplication.java
@@ -0,0 +1,12 @@
+
+package com.agriculturemanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgricultureManagementSystemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AgricultureManagementSystemApplication.class, args);
+    }
+}
